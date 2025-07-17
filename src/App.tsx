2859b0246cd31e60f4e5b3ae87740d89f828c9e1
iantodhunter <iantodhunter@gmail.com>
2025-07-17 14:45:32 +0200

    Fix blank screen issues

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+@@ .. @@
+ import { useState } from 'react';
+-import { StepOne } from './components/StepOne';
+-import { StepTwo } from './components/StepTwo';
+-import { DataReadinessStep } from './components/DataReadinessStep';
+-import { UXReadinessStep } from './components/UXReadinessStep';
++import { StepOne } from '../components/StepOne';
++import { StepTwo } from '../components/StepTwo';
++import { DataReadinessStep } from '../components/DataReadinessStep';
++import { UXReadinessStep } from '../components/UXReadinessStep';
+ 
+ type ResponseValue = 'yes' | 'no' | 'planned';
