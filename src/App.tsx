3cff03806500ad4ac91bdce41e3fd6b6b25a96cb
iantodhunter <iantodhunter@gmail.com>
2025-07-17 14:46:04 +0200

    Fix syntax errors and missing CSS file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-@@ .. @@
- import { useState } from 'react';
--import { StepOne } from './components/StepOne';
--import { StepTwo } from './components/StepTwo';
--import { DataReadinessStep } from './components/DataReadinessStep';
--import { UXReadinessStep } from './components/UXReadinessStep';
-+import { StepOne } from '../components/StepOne';
-+import { StepTwo } from '../components/StepTwo';
-+import { DataReadinessStep } from '../components/DataReadinessStep';
-+import { UXReadinessStep } from '../components/UXReadinessStep';
+import { useState } from 'react';
+import { StepOne } from './components/StepOne';
+import { StepTwo } from './components/StepTwo';
+import { DataReadinessStep } from './components/DataReadinessStep';
+import { UXReadinessStep } from './components/UXReadinessStep';
  
  type ResponseValue = 'yes' | 'no' | 'planned';
