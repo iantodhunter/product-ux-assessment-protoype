eac987b3ac4880c7132ae75dbb80e366ed1258e3
iantodhunter <iantodhunter@gmail.com>
2025-07-17 15:04:14 +0200

    Fix component import paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import { StepOne } from './components/StepOne';
-import { StepTwo } from './components/StepTwo';
-import { DataReadinessStep } from './components/DataReadinessStep';
-import { UXReadinessStep } from './components/UXReadinessStep';
+import { StepOne } from '../components/StepOne';
+import { StepTwo } from '../components/StepTwo';
+import { DataReadinessStep } from '../components/DataReadinessStep';
+import { UXReadinessStep } from '../components/UXReadinessStep';
 
 type ResponseValue = 'yes' | 'no' | 'planned';
 
